34403c2b63e6c3b84fa1b94f95d3c0ced12e8fdb
Thgesqui <thib.gesquiere@gmail.com>
2020-04-11 14:13:56 +0200

    test matje

--- test.py
+++ test.py
@@ -2,7 +2,11 @@ import pandas as pd
 import numpy as np
 
 #Create dataframes
-Type_To_Path = pd.DataFrame({"Job_Type": [1,2,3,4],"Path": [[3,1,2,5],[4,1,3],[2,5,1,4,3],[2,4,5]]})
-print(Type_To_Path.head())
 
-Processing_Times_Prob = pd.DataFrame({""})
+Processing_Times_Prob = pd.DataFrame({"Job_Type":[1,2,3,4],"Distribution_WS1":[[12,2],[15,2],[15,3],[0,0]]
+                                         ,"Distribution_WS2":[[20,4],[0,0],[21,3],[18,3]]
+                                         ,"Distribution_WS3":[[16,4],[14,2],[10,1],[0,0]]
+                                         ,"Distribtuion_WS4":[[0,0],[20,3],[24,4],[13,2]]
+                                         ,"Distribution_WS5":[[25,5],[0,0],[20,3],[25,5]]})
+
+print(Processing_Times_Prob)
