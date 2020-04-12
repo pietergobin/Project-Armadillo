462efac9e45891023d6f0b6b79f58cb871d86a12
Thgesqui <thib.gesquiere@gmail.com>
2020-04-12 11:16:56 +0200

    update

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 '''this file holds the structure for the simulation of a radiology department
 authors: '''
+
 import radiology_functions as f
 
-
-#initialise objects
 f.clock = 0
-
-Station_1 = f.Station(3,1)
+station_1 = f.Station(3,1)
 f.generate_arrival(True)
-event_queue = f.event_queue
-print(event_queue)
-
+queue = f.event_queue
+print(queue)
