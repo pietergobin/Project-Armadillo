4cdd3b54af861740fc8cffe14e31290214194a51
Thgesqui <thib.gesquiere@gmail.com>
2020-04-15 16:50:23 +0200

    test

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
 import pandas as pd
 import numpy as np
+import matplotlib as plt
+
+df = pd.read_csv("")
+
+
+
+plt.figure(fig_size=(4,3))
+plt.plot(df["run"],df["objective function"],label = "Objective_Function")
+plt.title("Running Averages per day")
+plt.legend(loc = "upper right")
+plt.show()
