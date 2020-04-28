b7bc97ee98ad0707f38e13af9170d6b07ffc9a92
Thgesqui <thib.gesquiere@gmail.com>
2020-04-28 11:05:09 +0200

    juiste RHO

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ authors: '''
 from radiology_functions import simulate
 
 #zet u naam hier dan hebben we mooi overzicht
-name = "(1,8) "
-min =1
+name = "(4,8) "
+min =4
 max =8
 simulate(name+'screening_scenario---',servers_of_2=min, servers_of_5=min, upgrade=0,number_of_runs=32, number_of_jobs=1000)
 simulate(name+'screening_scenario+--',servers_of_2=max, servers_of_5=min, upgrade=0,number_of_runs=32, number_of_jobs=1000)
