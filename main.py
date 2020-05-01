2571b2c772ae629803201b8ee2da264d534ffa19
Pieter <pieter.gobin@gmail.com>
2020-05-01 15:04:53 +0200

    fixed min max error

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from radiology_functions import simulate
 
 name = "(3,3) "
 min, max = 3,3
-simulate(name+' comparative upgrade',servers_of_2=min, servers_of_5=min, upgrade=1,number_of_runs=32, number_of_jobs=1000)
-simulate(name+'comparative new',servers_of_2=max, servers_of_5=min, upgrade=2,number_of_runs=32, number_of_jobs=1000)
+simulate(name+' comparative upgrade',servers_of_2=min, servers_of_5=max, upgrade=1,number_of_runs=32, number_of_jobs=1000)
+simulate(name+'comparative new',servers_of_2=min, servers_of_5=max, upgrade=2,number_of_runs=32, number_of_jobs=1000)
 
