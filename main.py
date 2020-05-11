178b8b381ce1224cd019845a8116941a639f29ec
pietergobin <pieter.gobin@gmail.com>
2020-05-11 17:56:08 +0200

    pairwise tralalala

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ authors: '''
 
 from radiology_functions import simulate
 
-name = "(4,3)"
-min = 4
+name = "(2,3)"
+min = 2
 max = 3
-simulate(name+'Comparative upgrade',servers_of_2=min, servers_of_5=max, upgrade=1,number_of_runs=32, number_of_jobs=1000)
-simulate(name+'Comparative new',servers_of_2=min, servers_of_5=max, upgrade=2,number_of_runs=32, number_of_jobs=1000)
+simulate(name+'upgrade 32',servers_of_2=min, servers_of_5=max, upgrade=1,number_of_runs=32, number_of_jobs=1000)
 
